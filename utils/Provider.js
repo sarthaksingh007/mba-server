36dc07f2ce59bbe860e6fef793b076be8c5e5ff4
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-10-23 00:01:23 +0530

    call back url change

--- utils/Provider.js
+++ utils/Provider.js
@@ -6,7 +6,7 @@ export const connectPassport = () => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:5500/api/v1/login",
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
     }, async function (accessToken, refreshToken, profile, done) {
         //Database comes here
 
