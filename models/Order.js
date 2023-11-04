53a2303be6c90bca19b7aa509d908a571ea15941
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-11-04 22:53:06 +0530

    Change in pincode

--- models/Order.js
+++ models/Order.js
@@ -18,7 +18,7 @@ const schema = new mongoose.Schema({
             type: String,
             required: true,
         },
-        pinCode: {
+        pincode: {
             type: Number,
             required: true,
         },
