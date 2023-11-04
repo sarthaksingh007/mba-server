c4d7f979802c4b4d727aed0de8886d8fafe9fc6f
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-11-04 19:11:16 +0530

    change in file

--- controllers/order.js
+++ controllers/order.js
@@ -1,173 +1,164 @@
-import { asyncError } from "../middleware/errorMiddleware.js";
+import { asyncError } from "../middlewares/errorMiddleware.js";
 import { Order } from "../models/Order.js";
+import { Payment } from "../models/Payment.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
 import { instance } from "../server.js";
-import crypto from "crypto"
-import { Payment } from '../models/Payment.js'
+import crypto from "crypto";
 
+export const placeOrder = asyncError(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingCharges,
+    totalAmount,
+  } = req.body;
 
-export const placeOrder = asyncError(
-    async (req, res, next) => {
-        const {
-            shippingInfo,
-            orderItems,
-            paymentMethod,
-            itemsPrice,
-            taxPrice,
-            shippingCharges,
-            totalAmount,
-        } = req.body;
+  const user = req.user._id;
 
-        const user = req.user._id;
+  const orderOptions = {
+    shippingInfo,
+    orderItems,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingCharges,
+    totalAmount,
+    user,
+  };
 
-        const orderOptions = {
-            shippingInfo,
-            orderItems,
-            paymentMethod,
-            itemsPrice,
-            taxPrice,
-            shippingCharges,
-            totalAmount,
-            user,
-        };
+  await Order.create(orderOptions);
 
-        await Order.create(orderOptions); //thats create the model
+  res.status(201).json({
+    success: true,
+    message: "Order Placed Successfully via Cash On Delivery",
+  });
+});
 
-        res.status(201).json({
-            success: true,
-            message: "Order palced successfully via Cash On Delivery",
-        });
-    });
+export const placeOrderOnline = asyncError(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingCharges,
+    totalAmount,
+  } = req.body;
 
+  const user = req.user._id;
 
+  const orderOptions = {
+    shippingInfo,
+    orderItems,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingCharges,
+    totalAmount,
+    user,
+  };
 
-export const placeOrderOnline = asyncError(
-    async (req, res, next) => {
-        const {
-            shippingInfo,
-            orderItems,
-            paymentMethod,
-            itemsPrice,
-            taxPrice,
-            shippingCharges,
-            totalAmount,
-        } = req.body;
+  const options = {
+    amount: Number(totalAmount) * 100,
+    currency: "INR",
+  };
+  const order = await instance.orders.create(options);
 
-        const user = req.user._id;
-
-        const orderOptions = {
-            shippingInfo,
-            orderItems,
-            paymentMethod,
-            itemsPrice,
-            taxPrice,
-            shippingCharges,
-            totalAmount,
-            user,
-        };
-
-
-        const options = {
-            amount: Number(totalAmount) * 100,  // amount in the smallest currency unit
-            currency: "INR",
-        };
-        const order = await instance.orders.create(options); //thats create the model
-
-
-        res.status(201).json({
-            success: true,
-            order,
-            orderOptions,
-        });
-    }
-);
+  res.status(201).json({
+    success: true,
+    order,
+    orderOptions,
+  });
+});
 
 export const paymentVerification = asyncError(async (req, res, next) => {
+  const {
+    razorpay_payment_id,
+    razorpay_order_id,
+    razorpay_signature,
+    orderOptions,
+  } = req.body;
 
-    const { razorpay_payment_id,
-        razorpay_order_id,
-        razorpay_signature,
-        orderOptions
-    } = req.body;
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
 
-    const body = razorpay_order_id + "|" + razorpay_payment_id;
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(body)
+    .digest("hex");
 
-    const expectedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_API_SECRET).update(body).digest("hex");
+  const isAuthentic = expectedSignature === razorpay_signature;
 
-    const isAuthentic = expectedSignature === razorpay_signature;
+  if (isAuthentic) {
+    const payment = await Payment.create({
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature,
+    });
 
-    if (isAuthentic) {
-        const payment = await Payment.create({
-            razorpay_order_id,
-            razorpay_payment_id,
-            razorpay_signature,
-        })
+    await Order.create({
+      ...orderOptions,
+      paidAt: new Date(Date.now()),
+      paymentInfo: payment._id,
+    });
 
-        await Order.create({
-            ...orderOptions,
-            paidAt:new Date(Date.now()),
-            paymentInfo:payment._id,
-        })
+    res.status(201).json({
+      success: true,
+      message: `Order Placed Successfully. Payment ID: ${payment._id}`,
+    });
+  } else {
+    return next(new ErrorHandler("Payment Failed", 400));
+  }
+});
 
-         res.status(201).json({
-            success:true,
-            message:`Order Placed Successfully. Payment ID: ${payment._id}`,
-         })
-    }
-    else {
-        return next(new ErrorHandler("Payment Failed", 400));
-    }
+export const getMyOrders = asyncError(async (req, res, next) => {
+  const orders = await Order.find({
+    user: req.user._id,
+  }).populate("user", "name");
 
-})
-
-
-
-export const getMyorders = asyncError(async (req, res, next) => {
-    const orders = await Order.find({
-        user: req.user._id,
-    }).populate("user", "name");
-    res.status(200).json({
-        success: true,
-        orders,
-    })
-})
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
 
 export const getOrderDetails = asyncError(async (req, res, next) => {
-    const order = await Order.findById(req.params.id).populate("user", "name");
+  const order = await Order.findById(req.params.id).populate("user", "name");
 
-    if (!order) return next(new ErrorHandler("Invalid Order Id", 404));
+  if (!order) return next(new ErrorHandler("Invalid Order Id", 404));
 
-    res.status(200).json({
-        success: true,
-        order,
-    });
-})
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
 
 export const getAdminOrders = asyncError(async (req, res, next) => {
-    const orders = await Order.find({}).populate("user", "name");
-    res.status(200).json({
-        success: true,
-        orders,
-    })
-})
+  const orders = await Order.find({}).populate("user", "name");
 
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
 export const processOrder = asyncError(async (req, res, next) => {
-    console.log(req.params.id);
-    const order = await Order.findById(req.params.id)
+  const order = await Order.findById(req.params.id);
 
-    if (!order) return next(new ErrorHandler("Invalid Order Id", 404));
+  if (!order) return next(new ErrorHandler("Invalid Order Id", 404));
 
-    if (order.orderStatus === "Preparing") order.orderStatus = "Shipped";
-    else if (order.orderStatus === "Shipped") {
-        order.orderStatus = "Delivered";
-        order.deliveredAt = new Date(Date.now());
-    }
-    else if (order.orderStatus === "Delivered") {
-        return next(new ErrorHandler("Food already delivered", 400));
-    }
-    await order.save();
-    res.status(200).json({
-        success: true,
-        message: "Status updated successfully",
-    })
-})
+  if (order.orderStatus === "Preparing") order.orderStatus = "Shipped";
+  else if (order.orderStatus === "Shipped") {
+    order.orderStatus = "Delivered";
+    order.deliveredAt = new Date(Date.now());
+  } else if (order.orderStatus === "Delivered")
+    return next(new ErrorHandler("Food Already Delivered", 400));
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Status Updated Successfully",
+  });
+});
