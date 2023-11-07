6549ec0afa61c5e6554672c43cd9d76de5fa4834
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-11-07 18:50:27 +0530

    change the name of the routes of the order to orders

--- routes/order.js
+++ routes/order.js
@@ -18,9 +18,9 @@ router.get("/myorders", isAuthenticated, getMyOrders)
 router.get("/order/:id", isAuthenticated, getOrderDetails)
 
 //add admin middleware
-router.get("/admin/order", isAuthenticated, authorizeAdmin, getAdminOrders);
+router.get("/admin/orders", isAuthenticated, authorizeAdmin, getAdminOrders);
 
 
-router.get("/admin/order/:id", isAuthenticated, authorizeAdmin, processOrder);
+router.get("/admin/orders/:id", isAuthenticated, authorizeAdmin, processOrder);
 
 export default router;
